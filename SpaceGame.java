22384f180f7ab615924e14becde89deb90a7ab40
JoshuaRodrigues <50383549+JoshuaRodrigues@users.noreply.github.com>
2019-06-11 18:02:22 +0500

    Update SpaceGame.java

--- SpaceGame.java
+++ SpaceGame.java
@@ -1,19 +1,21 @@
-import javax.swing.JFrame;
+import javax.swing.JFrame;//importing JFrame
 
 public class SpaceGame{
-  JFrame frame;
-  AlienAttack alienAttack;
+  JFrame frame;//the frame
+  AlienAttack alienAttack;//the game instance
+  
+  //constructor
   public SpaceGame(){
     frame = new JFrame("Space Shooter");
     alienAttack = new AlienAttack();
-    frame.setBounds(320, 25, 800, 800);
-    frame.setResizable(false);
-    frame.setVisible(true);
-    frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-    frame.add(alienAttack);
+    frame.setBounds(320, 25, 800, 800);//size of frame
+    frame.setResizable(false);//frame does not change size
+    frame.setVisible(true);//frame is visible
+    frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);//all frames exit on close
+    frame.add(alienAttack);//adds game panel to frame
   }
   
   public static void main(String[] args){
-    new SpaceGame();
+    new SpaceGame();//makes a new instance of the game
   }
 }
